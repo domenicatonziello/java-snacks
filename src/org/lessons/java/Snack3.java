f4867a882fc8f5e1a1150bb46b1098b85a992ebf
domenicatonziello <domenica.tonziello10@gmail.com>
2023-05-05 11:43:34 +0200

    snack3

--- src/org/lessons/java/Snack3.java
+++ src/org/lessons/java/Snack3.java
@@ -1,5 +1,23 @@
 package org.lessons.java;
 
+import java.util.Arrays;
+
 public class Snack3 {
+	public static void main(String[] args) {
+		Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8};
+		
+		 // int getIndex = Arrays.asList(numbers).indexOf(7);
+		 
+		 int sum = 0;
+
+	        for(int i = 0; i < numbers.length ; i++) {
+	        	if(Arrays.asList(numbers).indexOf(i) % 2 != 0) {
+	        		sum += numbers[i];
+	        	}
+	        }
+	        
+	        System.out.println(sum);
+		
+	}
 
 }
